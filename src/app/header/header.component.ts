70e506c9dd32254e96d2f55cfd4f1611cb7d7f03
mostela_amd <joao-mostela@hotmail.com>
2020-01-02 23:31:13 -0300

    Add Carrocel na header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { PoSlideItem } from '@portinari/portinari-ui';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.less']
+})
+
+export class HeaderComponent implements OnInit {
+
+  height: number;
+  interval: number;
+  slideItem: PoSlideItem;
+  slideItems: Array<PoSlideItem>;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.restore();
+  }
+
+  restore() {
+    this.interval = undefined;
+    this.height = undefined;
+    this.slideItems = [
+      { image: 'https://www.causaoperaria.org.br/wp-content/uploads/2019/07/Portinari-1024x585.jpg'},
+      { image: 'https://static.todamateria.com.br/upload/ro/me/romero-britto-og.jpg'}
+    ];
+  }
+}
