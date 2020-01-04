0b81da23d2fefcdab9a5f6c4c9be97ed45d17720
mostela_amd <joao-mostela@hotmail.com>
2020-01-04 13:29:16 -0300

    Criado cards de promoção

--- src/app/navbar/menu.ts
+++
@@ -1,7 +0,0 @@
-export const MeuMenu = [
-  {"link":"cardapio","titulo":"cardapio"},
-  {"link":"#","titulo":"bebidas"},
-  {"link":"#","titulo":"promoção"},
-  {"link":"#","titulo":"serviços"},
-  {"link":"#","titulo":"contato"}
-]
